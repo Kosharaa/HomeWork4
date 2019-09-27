9b4eac9be9a97ea82952a905eb9f650a883517e7
Koshara <ukracent2009@gmail.com>
2019-09-27 20:59:41 +0300

    first commit for DZ4

---
+++ ToUpperCase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter you'r Second Name, Name, Surname: ");
+            string FirstName = Console.ReadLine();
+            string[] SubName = FirstName.Split(' ');
+
+            for (int i = 0; i < SubName.Count(); i++)
+            {
+                FirstName = SubName[i].Substring(0, 1).ToUpper();
+                if (i == 0)
+                {
+                    SubName[i] = FirstName + SubName[i].Substring(1, SubName[i].Length - 1);
+                }
+                else
+                {
+                    SubName[i] = FirstName + ".";
+                }
+            }
+            Console.WriteLine(string.Join(" ", SubName));
+
+            /* Более короткая версия
+            string fio = Console.ReadLine();
+            string[] array = fio.Split(' ').Select(x => x.Substring(0, 1).ToUpper() + x.Remove(0, 1)).ToArray();
+            Console.WriteLine($"{array.First()} {array[1].First()}.{array[2].First()}.");
+            */
+
+            /* хороший метод, но требует дополнительного пространства имен System.Globalization - забракован учителем.
+            FirstName = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(FirstName.ToLower());
+            Console.WriteLine(FirstName);
+            */
+            Console.ReadKey();
+        }
+    }
+}
---
+++ mass[][].cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] NameArray = new string[3][];
+            Console.WriteLine("Enter you'r First Name, Surname, Second Name: ");
+
+            for (int i = 0; i < NameArray.Length; i++)
+            {                
+                    string Info = Console.ReadLine();
+                    NameArray[i] = Info.Split(' ');                
+            }
+            Console.WriteLine("Looking someone?: ");
+            string search = Console.ReadLine();
+            Console.WriteLine("Search result:");
+            for (int i = 0; i < NameArray.Length; i++)
+            {
+                if (string.Join(" ", NameArray[i]).Contains(search))
+                Console.WriteLine(string.Join(" ", NameArray[i]));
+            }
+            /*
+            for (int i = 0; i < NameArray.Length; i++)
+            {
+                for (int j = 0; j < NameArray[i].Length; j++)
+                {
+                    Console.WriteLine(NameArray[i][j] + ",");
+                }
+                Console.WriteLine();
+            }
+            */
+
+
+
+
+            /*
+            string[][] NameArray = new string[3][];
+
+            Console.WriteLine("Enter you'r First Name, Surname, Second Name: ");
+            string FirtsBlock = Console.ReadLine();
+            NameArray[0] = FirtsBlock.Split(' ');
+            string SecondBlock = Console.ReadLine();
+            NameArray[1] = FirtsBlock.Split(' ');
+            string ThirdBlock = Console.ReadLine();
+            NameArray[2] = FirtsBlock.Split(' ');
+
+            for (int i = 0; i < NameArray.Length; i++)
+            {
+                for (int j = 0; j < NameArray[i].Length; j++)
+                {
+                    Console.WriteLine(NameArray[i][j] + ",");
+                }
+                Console.WriteLine();
+            }
+            */
+            Console.ReadKey();
+        }
+    }
+}
